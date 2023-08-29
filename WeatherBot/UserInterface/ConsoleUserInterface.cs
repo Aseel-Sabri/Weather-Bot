3194f7d8315cef48b2b2c195a85131934f42807e
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-08-29 15:55:21 +0300

    Add user interface classes

---
+++ WeatherBot/UserInterface/ConsoleUserInterface.cs
@@ -0,0 +1,52 @@
+﻿using WeatherBot.WeatherServices;
+
+namespace WeatherBot.UserInterface;
+
+public class ConsoleUserInterface : IUserInterface
+{
+    private readonly IWeatherServices _weatherServices;
+
+    public ConsoleUserInterface(IWeatherServices weatherServices)
+    {
+        _weatherServices = weatherServices;
+    }
+
+    public void Run()
+    {
+        var input = GetWeatherDataInput();
+        while (!IsQuitCommand(input))
+        {
+            var updateWeatherResult = _weatherServices.UpdateWeather(input);
+            if (updateWeatherResult.IsFailed)
+            {
+                foreach (var error in updateWeatherResult.Errors)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+
+            Console.WriteLine();
+            input = GetWeatherDataInput();
+        }
+    }
+
+    private string GetWeatherDataInput()
+    {
+        Console.WriteLine("Enter weather data:");
+        string? input;
+        do
+        {
+            input = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(input));
+
+        Console.WriteLine();
+        return input!.Trim();
+    }
+
+    private bool IsQuitCommand(string input)
+    {
+        const StringComparison stringComparisionCulture = StringComparison.CurrentCultureIgnoreCase;
+        return input.Equals("q", stringComparisionCulture)
+               || input.Equals("quit", stringComparisionCulture);
+    }
+}
