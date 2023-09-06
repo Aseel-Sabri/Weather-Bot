b8c811e7018e623aa6566ab1c3b3a73ceee5959e
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-09-06 11:17:18 +0300

    Add tests for ParserProvider

---
+++ WeatherBot.Tests/WeatherParsersTests/ParserProviderTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Moq;
+using WeatherBot.WeatherParsers;
+
+namespace WeatherBot.Tests.WeatherParsersTests;
+
+public class ParserProviderTests
+{
+    [Theory]
+    [AutoMoqData]
+    public void ShouldFailWhenNoSuitableParser(
+        [Frozen] IEnumerable<IWeatherParser> weatherParsers,
+        ParserProvider parserProvider)
+    {
+        weatherParsers.ToList().ForEach(weatherParser =>
+            Mock.Get(weatherParser)
+                .Setup(p => p.IsSupportedFormat(It.IsAny<string>()))
+                .Returns(false));
+        const string errorMessage = "Invalid Format";
+
+        var result = parserProvider.GetSuitableParser(It.IsAny<string>());
+
+        result.IsFailed.Should().BeTrue();
+        result.Errors.Should().Contain(x => x.Message == errorMessage);
+    }
+
+
+    [Theory]
+    [AutoMoqData]
+    public void ShouldReturnSuitableParserWhenFound(
+        [Frozen] IEnumerable<IWeatherParser> weatherParsers,
+        ParserProvider parserProvider)
+    {
+        var suitableWeatherParser = weatherParsers.First();
+
+        Mock.Get(suitableWeatherParser)
+            .Setup(p => p.IsSupportedFormat(It.IsAny<string>()))
+            .Returns(true);
+
+        var result = parserProvider.GetSuitableParser(It.IsAny<string>());
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(suitableWeatherParser);
+    }
+}
